987ae2f7296e24be2c29c8e2e453e6a289ee3e3a
DavidIzhaki <davidizhakinew@gmail.com>
2025-03-03 15:41:14 +0200

    Red car works

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,16 +1,16 @@
-use std::time;
-use search_core::search::solve::solve_problem;
+use search_core::problems::compute_number_problem::compute_number_problem::ComputeNumberProblem;
 use search_core::problems::farm_problem::farm_problem::FarmProblem;
 use search_core::problems::market_trader_problem::market_trader_problem::MarketTraderProblem;
-use search_core::problems::compute_number_problem::compute_number_problem::ComputeNumberProblem;
-use search_core::problems::compute_number_problem::compute_number_problem::RedCarProblem;
+use search_core::problems::red_car_problem::red_car_problem::RedCarProblem;
+use search_core::search::solve::solve_problem;
+use std::time;
 use time::Instant;
 
 fn main() {
     let start_time = Instant::now();
-    solve_problem::<MarketTraderProblem>(
-        "search_core/src/inputs/example_inputs/market_trader_problem/input_1.json",
-        "GBFS",
+    solve_problem::<RedCarProblem>(
+        "search_core/src/inputs/example_inputs/red_car_problem/Red_Car_Problem1_Beginner.json",
+        "A*",
     );
 
     let elapsed_time = start_time.elapsed(); // Calculate elapsed time
