dfee4f76ee8eefa6be734b3995ba8131985e6a08
DavidIzhaki <davidizhakinew@gmail.com>
2025-03-04 14:51:26 +0200

    Adding 2 domains One for counter with linear, one for counter with expression Adding problems too

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,16 +1,18 @@
 use search_core::problems::compute_number_problem::compute_number_problem::ComputeNumberProblem;
+use search_core::problems::counters_expression_problem::counters_expression_problem::CountersExpressionProblem;
+use search_core::problems::counters_problem::counters_problem::CountersProblem;
 use search_core::problems::farm_problem::farm_problem::FarmProblem;
+use search_core::problems::foo_sailing_problem::foo_sailing_problem::FooSailingProblem;
 use search_core::problems::market_trader_problem::market_trader_problem::MarketTraderProblem;
 use search_core::problems::red_car_problem::red_car_problem::RedCarProblem;
-use search_core::problems::foo_sailing_problem::foo_sailing_problem::FooSailingProblem;
 use search_core::search::solve::solve_problem;
 use std::time;
 use time::Instant;
 
 fn main() {
     let start_time = Instant::now();
-    solve_problem::<FooSailingProblem>(
-        "search_core/src/inputs/example_inputs/foo_sailing_problem/pfile1.json",
+    solve_problem::<CountersProblem>(
+        "search_core/src/inputs/example_inputs/counters_problem/pfile1.json",
         "A*",
     );
 
