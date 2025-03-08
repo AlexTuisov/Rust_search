69459930d723b271cb41c13be5f7f4f16de26512
DavidIzhaki <davidizhakinew@gmail.com>
2025-03-08 17:00:11 +0200

    Start by fixing the pddl domain

--- search_core/src/problems/sugar_problem/sugar_problem.rs
+++ search_core/src/problems/sugar_problem/sugar_problem.rs
@@ -1,1 +1,25 @@
+pub struct State{
 
+}
+impl State{
+
+}
+impl StateTrait for State{
+
+}
+pub struct Truck{
+
+}
+
+pub struct Carne{
+
+}
+
+pub struct Mill{
+
+} 
+pub struct Depot{
+
+}
+
+pub st
